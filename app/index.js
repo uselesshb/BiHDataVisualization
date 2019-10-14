7de5f2de3c3291f82d7ed11f8ff691c13055f52f
uselesshb <>
2019-10-14 17:04:12 +0200

    use papa parser for csv

--- app/index.js
+++ app/index.js
@@ -40,74 +40,65 @@ window.addEventListener('DOMContentLoaded', (event) => {
 });
 
 function handleDataFileSelect(evt) {
-    const file = evt.target.files[0];
+    clearFeatures();
 
-    const reader = new FileReader();
-
-    reader.addEventListener("load", function(e) {
-        //clear features from vector source
-        const features = map.getLayers().getArray()[1].getSource().getFeatures();
-        for(let i = 0; i < features.length; i++){
-            features[i].unset("data", true);
-        }
-        map.getLayers().getArray()[1].getSource().clear();
-
-        //read rows from data file and add the requested features to vector source
-        const data_rows = e.target.result.split(/\r?\n/);
-        for(let i = 0; i < data_rows.length; i++){
-            const row = data_rows[i].split(";");
-            let found = false;
-            for(let j = 0; j < all_features.length; j++){
-                if(all_features[j].get("name") == row[0]){
-                    const feature = all_features[j];
-                    const style = new ol.style.Style();
-                    const stroke = new ol.style.Stroke({color: "grey", width: 1.25});
-                    style.setStroke(stroke);
-                    feature.setStyle(style);
-                    feature.set("data", parseInt(row[1]), true);
-                    map.getLayers().getArray()[1].getSource().addFeature(feature);
-                    found = true;
-                    break;
+    Papa.parse(evt.target.files[0], {
+        complete: function(results) {
+            const data = results.data;
+            for(let i = 0; i < data.length; i++){
+                let found = false;
+                for(let j = 0; j < all_features.length; j++){
+                    if(all_features[j].get("name") == data[i][0]){
+                        const feature = all_features[j];
+                        const style = new ol.style.Style();
+                        const stroke = new ol.style.Stroke({color: "grey", width: 1.25});
+                        style.setStroke(stroke);
+                        feature.setStyle(style);
+                        feature.set("data", parseInt(data[i][1]), true);
+                        map.getLayers().getArray()[1].getSource().addFeature(feature);
+                        found = true;
+                        break;
+                    }
+                }
+                if(!found){
+                    console.log("Unknown feature: " + data[i][0]);
                 }
             }
-            if(!found){
-                console.log("Unknown feature: " + row[0]);
-            }
-        }
+        },
+        delimiter: ";"
     });
-
-    reader.readAsText(file);
 }
 
 function handleStyleFileSelect(evt) {
-    const file = evt.target.files[0];
-
-    const reader = new FileReader();
-
-    reader.addEventListener("load", function(e) {
-        //read rows from style file and change fill colors for features in vector source
-        const style_rows = e.target.result.split(/\r?\n/);
-        const features = map.getLayers().getArray()[1].getSource().getFeatures();
-        for(let j = 0; j < features.length; j++){
-            let found = false;
-            for(let i = 0; i < style_rows.length; i++){
-                const row = style_rows[i].split(";");
-                if(features[j].get("data") > parseInt(row[0]) && features[j].get("data") <= parseInt(row[1])){
-                    console.log(typeof(features[j].get("data")));
-                    const fill = new ol.style.Fill({color: row[2]});
+    Papa.parse(evt.target.files[0], {
+        complete: function(results) {
+            const data = results.data;
+            const features = map.getLayers().getArray()[1].getSource().getFeatures();
+            for(let j = 0; j < features.length; j++){
+                let found = false;
+                for(let i = 0; i < data.length; i++){
+                    if(features[j].get("data") > parseInt(data[i][0]) && features[j].get("data") <= parseInt(data[i][1])){
+                        const fill = new ol.style.Fill({color: data[i][2]});
+                        features[j].getStyle().setFill(fill);
+                        found = true;
+                        break;
+                    }
+                }
+                if(!found){
+                    const fill = new ol.style.Fill({color: "rgba(0,0,0,1)"});
                     features[j].getStyle().setFill(fill);
-                    found = true;
-                    break;
                 }
             }
-            if(!found){
-                const fill = new ol.style.Fill({color: "rgba(0,0,0,1)"});
-                features[j].getStyle().setFill(fill);
-            }
-        }
-
-        map.getLayers().getArray()[1].getSource().changed();
+            map.getLayers().getArray()[1].getSource().changed();
+        },
+        delimiter: ";"
     });
+}
 
-    reader.readAsText(file);
+function clearFeatures(){
+    const features = map.getLayers().getArray()[1].getSource().getFeatures();
+    for(let i = 0; i < features.length; i++){
+        features[i].unset("data", true);
+    }
+    map.getLayers().getArray()[1].getSource().clear();
 }
