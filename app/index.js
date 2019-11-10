05461fa6f6268dee42a7682722505f243f2a3da1
uselesshb <>
2019-11-10 17:34:25 +0100

    adding validation checks

--- app/index.js
+++ app/index.js
@@ -36,16 +36,19 @@ const dataWizardTexts = [];
 const styleWizardTexts = [];
 
 window.addEventListener('DOMContentLoaded', (event) => {
+    //initialize all_features array using areas data
     for(let i = 0; i < areas.length; i++)
     {
         const feature = new ol.Feature({geometry: new ol.geom.MultiPolygon(areas[i].polygons)});
-        feature.set("name", areas[i]["name"], true);
-        feature.set("admin_level", areas[i]["admin_level"], true);
+        feature.set("name", areas[i].name, true);
+        feature.set("admin_level", areas[i].admin_level, true);
         all_features.push(feature);
     }
 
+    //add the first feature (BiH) to map
     map.getLayers().getArray()[1].getSource().addFeature(all_features[0]);
 
+    //get dynamic labels
     let texts = document.querySelectorAll(".activate_on_data_wizard");
     texts.forEach(element => {
         dataWizardTexts.push(element);
@@ -56,6 +59,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
         styleWizardTexts.push(element);
     });
 
+    //add listener to "choose file" button
     const input = document.querySelector("#wizard_file_input");
     input.addEventListener('change', handleFileSelect);
 });
@@ -73,22 +77,27 @@ function handleDataFileSelect(evt) {
         complete: function(results) {
             const data = results.data;
             for(let i = 0; i < data.length; i++) {
-                let found = false;
-                for(let j = 0; j < all_features.length; j++) {
-                    if (data[i].length > 1 && all_features[j].get("name") == data[i][0]) {
-                        const feature = all_features[j];
-                        const style = new ol.style.Style();
-                        const stroke = new ol.style.Stroke({color: "grey", width: 1.25});
-                        style.setStroke(stroke);
-                        feature.setStyle(style);
-                        feature.set("data", parseInt(data[i][1]), true);
-                        map.getLayers().getArray()[1].getSource().addFeature(feature);
-                        found = true;
-                        break;
+                //validation check
+                let line_valid = false;
+                let line_data = data[i];
+                let feature;
+                if (line_data.length > 1) {
+                    feature = all_features.find((f) => f.get("name") == line_data[0]);
+                    if (feature) {
+                        if (isValidInt(line_data[1])) {
+                            line_valid = true;
+                        }
                     }
                 }
-                if (!found) {
-                    console.log("Unknown feature: " + data[i][0]);
+                
+                if (line_valid) {
+                    //correct feature found, add it to the map
+                    const style = new ol.style.Style();
+                    const stroke = new ol.style.Stroke({color: "grey", width: 1.25});
+                    style.setStroke(stroke);
+                    feature.setStyle(style);
+                    feature.set("data", parseInt(line_data[1]), true);
+                    map.getLayers().getArray()[1].getSource().addFeature(feature);
                 }
             }
             setCurrentWizard(wizardType.STYLE);
@@ -101,25 +110,49 @@ function handleStyleFileSelect(evt) {
     Papa.parse(evt.target.files[0], {
         complete: function(results) {
             const data = results.data;
-            const features = map.getLayers().getArray()[1].getSource().getFeatures();
-            for(let i = 0; i < features.length; i++) {
-                let found = false;
-                for(let j = 0; j < data.length; j++) {
-                    if (data[j].length > 2 && features[i].get("data") > parseInt(data[j][0]) && features[i].get("data") <= parseInt(data[j][1])) {
-                        const fill = new ol.style.Fill({color: data[j][2]});
-                        features[i].getStyle().setFill(fill);
-                        found = true;
-                        break;
+            //validation check
+            let lines_valid = true;
+            for(let i = 0; i < data.length; i++) {
+                let line_data = data[i];
+                let line_valid = false;
+                if(line_data.length > 2)
+                {
+                    if (isValidInt(line_data[0])) {
+                        if (isValidInt(line_data[1])) {
+                            if (isValidRGBAValue(line_data[2])) {
+                                line_valid = true;
+                            }
+                        }
                     }
                 }
-                if (!found) {
-                    console.log("No style for feature: " + features[i].get("name"));
-                    const fill = new ol.style.Fill({color: "rgba(0,0,0,1)"});
-                    features[i].getStyle().setFill(fill);
+                if (!line_valid) {
+                    line_valid = false;
+                    break;
                 }
             }
-            map.getLayers().getArray()[1].getSource().changed();
-            setCurrentWizard(wizardType.NONE);
+
+            if (lines_valid) {
+                const features = map.getLayers().getArray()[1].getSource().getFeatures();
+                for(let i = 0; i < features.length; i++) {
+                    let found = false;
+                    for(let j = 0; j < data.length; j++) {
+                        let line_data = data[j];
+                        if (features[i].get("data") > parseInt(line_data[0]) && features[i].get("data") <= parseInt(line_data[1])) {
+                            //styling for this feature found, apply
+                            const fill = new ol.style.Fill({color: line_data[2]});
+                            features[i].getStyle().setFill(fill);
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found) {
+                        const fill = new ol.style.Fill({color: "rgba(0,0,0,1)"});
+                        features[i].getStyle().setFill(fill);
+                    }
+                }
+                map.getLayers().getArray()[1].getSource().changed();
+                setCurrentWizard(wizardType.NONE);
+            }
         },
         delimiter: ";"
     });
@@ -179,4 +212,37 @@ function setWizardTexts(wizard) {
             element.style.display = "none";
         }
     });
+}
+
+function isValidRGBAValue(s) {
+    const p = /rgba\((\d+),(\d+),(\d+),(\d\.\d+)\)/;
+    const result = s.match(p);
+    if (result != null && result.length == 5) {
+        let rgb_values_ok = true;
+        let a_value_ok = false;
+        for (let i = 1; i < 5; i++) {
+            const x = parseInt(result[i]);
+            if (x < 0 && x > 255) {
+                rgb_values_ok = false;
+            }
+        }
+
+        const a = parseFloat(result[4]);
+        if(a >= 0.0 && a <= 1.0) {
+            a_value_ok = true;
+        }
+        
+        if (rgb_values_ok && a_value_ok) {
+            return true;
+        }
+        
+    }
+    return false;
+}
+
+function isValidInt(s) {
+    if (!isNaN(parseInt(s))) {
+        return true;
+    }
+    return false;
 }
